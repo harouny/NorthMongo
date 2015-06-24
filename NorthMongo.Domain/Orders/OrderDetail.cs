47d043b781c825697ff7abb3f5a301e6b35ae0ee
harouny <ahmedelharouny@outlook.com>
2015-06-24 21:44:47 +1000

    renaming a file

--- NorthMongo.Domain/Orders/OrderDetail.cs
+++ NorthMongo.Domain/Orders/OrderDetail.cs
@@ -7,8 +7,5 @@ namespace NorthMongo.Domain.Orders
         public decimal UnitPrice { get; set; }
         public short Quantity { get; set; }
         public float Discount { get; set; }
-    
-        //public virtual Order Order { get; set; }
-        //public virtual Product Product { get; set; }
     }
 }
